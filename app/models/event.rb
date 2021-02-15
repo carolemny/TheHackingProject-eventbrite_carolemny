1be832d5edfd0b76a5a7989b5cb3be82483198a3
Carole Meney <meneycarole@gmail.com>
2021-02-15 20:41:22 +0100

    Add Mailer

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,19 +1,14 @@
 class Event < ApplicationRecord
   validates :start_date, presence: true, if: :start_date_is_possible?
-  validates :duration, presence: true,  numericality: { only_integer: true, greater_than: 0 }, if: :duration_is_multiple_of_five?
+  validates :duration, presence: true,  numericality: { only_integer: true, greater_than: 0, :modulo => 5 }
   validates :title, presence: true, length: { in: 5..140 }
   validates :description, presence: true, length: { in: 20..1000 }
   validates :price, presence: true,  numericality: { only_integer: true, greater_than: 0, less_than: 1000 }
-  validates :location, presence: true
+  validates :location, presence: true 
 
   def start_date_is_possible?
     #Afficher message d'erreur à moins que "Date début > Date Aujourd'hui"
     errors.add(:start_date, "You can't create event in the past") unless start_date > DateTime.now
-  end
-
-  def duration_is_multiple_of_five?
-    #Afficher message d'erreur à moins que "durée soit un multiple de 5"
-    errors.add(:duration, "Must be a multiple of five!") unless duration % 5 == 0
   end
 
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  #after_create :welcome_send
+  after_create :welcome_send
 
   has_many :attendances
   has_many :events, through: :attendances
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   #has_many :admin_events, foreign_key: "admin_id", class_name: "Event"
   #has_many :attendances, foreign_key: 'attendee_id'
   #has_many :attendees_events, foreign_key: "attendee_id", class_name: "Event", through: :attendances
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
