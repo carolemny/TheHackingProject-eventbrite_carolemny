0b105e7e2ef153431fc172d9bef992c4af447be1
Carole Meney <meneycarole@gmail.com>
2021-02-16 00:01:08 +0100

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,10 @@
   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Lorem.paragraph(sentence_count: 3, supplemental: true), encrypted_password: "password", email: Faker::Internet.email(domain: 'yopmail.com'))
 end
 
-5.times do 
-  Attendance.create(stripe_customer_id: "stringtest")
+2.times do
+  Event.create(start_date: Faker::Time.between(from: DateTime.now, to: DateTime.now + 60), duration: Faker::Number.number(digits: 10), title: Faker::Lorem.characters, description: Faker::Lorem.characters, price: Faker::Number.within(range: 1..1000), location: Faker::Address.city, user_id: 1)
 end
 
-2.times do
-  Event.create(start_date: Faker::Time.between(from: DateTime.now, to: DateTime.now + 60), duration: Faker::Number.number(digits: 10), title: Faker::Lorem.characters, description: Faker::Lorem.characters, price: Faker::Number.within(range: 1..1000), location: Faker::Address.city)
+5.times do 
+  Attendance.create(stripe_customer_id: "stringtest", user_id: 1, event_id: 1)
 end
